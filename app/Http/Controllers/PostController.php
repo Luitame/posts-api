3d9c90cffa94b38ba3c09edae6b79adbb6541367
Luítame de Oliveira <ldeoliveiras@falabella.com>
2022-06-27 09:31:05 -0300

    build: first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
+
+class PostController extends Controller
+{
+    const BASE_URI = 'https://www.scalablepath.com/api/test/';
+
+    public function index()
+    {
+        if (count(User::get()) == 0) {
+            $users = Http::get(self::BASE_URI . 'test-users')->json();
+
+            foreach ($users as $user) {
+                User::create([
+                    'id' => $user['id'],
+                    'name' => $user['name'],
+                    'email' => $user['email'],
+                    'username' => $user['username'],
+                    'password' => Hash::make(Str::uuid()),
+                ]);
+            }
+
+            $posts = Http::get(self::BASE_URI . 'test-posts')->json();
+
+            foreach ($posts as $post) {
+                Post::create([
+                    'id' => $post['id'],
+                    'title' => $post['title'],
+                    'body' => $post['body'],
+                    'user_id' => $post['userId'],
+                ]);
+            }
+        }
+
+        $posts = Post::get();
+
+        return $posts;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
