d5d21c9c72c4d251d2e7322e26d82a4040711904
Luítame de Oliveira <ldeoliveiras@falabella.com>
2022-06-27 17:00:58 -0300

    feature: adding a cache layer to the posts endpoint

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,19 @@
 
 namespace App\Providers;
 
+use App\Models\Post;
+use App\Observers\PostObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
+    public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+    public function boot(): void
     {
-        //
+        Post::observe(PostObserver::class);
     }
 }
