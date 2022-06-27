875e2bdd296eba751f5e2ff64bc4c2ee58a44f88
Luítame de Oliveira <ldeoliveiras@falabella.com>
2022-06-27 17:13:50 -0300

    build: moving all API controllers onto Http\Api folder and applying some style

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,9 +12,14 @@ class Post extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
-      'title',
-      'body',
-      'user_id'
+        'title',
+        'body',
+        'user_id'
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
     ];
 
     public function user(): BelongsTo
